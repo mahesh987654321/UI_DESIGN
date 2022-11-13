e9b855c1ed202905bee50de57d5f0fc67dffbaf1
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-11-13 15:08:34 +0600

    possibility

--- src/components/feture/Feture.jsx
+++ src/components/feture/Feture.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./Feture.css";
-const Feture = () => {
-  return <div>Feture</div>;
+const Feture = ({ title, text }) => {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <h1>{title}</h1>
+        <div />
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Feture;
